88fc1c676d7aceee7d402478d0cae89fc48c3d55
Stephan Lotter <stephan@neurasoft.co.za>
2020-05-22 10:04:54 +0200

    Added Pluralization helper and unit test.

---
+++ Oqtane.Shared/Shared/Pluralization.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Oqtane.Shared 
+{
+    public static class Pluralization 
+    {
+        public static string Pluralize(string singular)
+        {
+            return
+                string.IsNullOrWhiteSpace(singular)
+                    ? ""
+                    : singular.EndsWith("y")
+                        ? PluralizeY(singular)
+                        : singular + "s";
+        }
+
+        private static string PluralizeY(string singular) 
+        {
+            return Regex.Replace(singular, @"y$", "ies");
+        }
+    }
+
+}
---
+++ Oqtane.Test/Oqtane.Shared.Tests/PluralizationTests.cs
@@ -0,0 +1,25 @@
+﻿using Oqtane.Shared;
+using Xunit;
+
+namespace Oqtane.Test.Oqtane.Shared.Tests
+{
+    public class PluralizationTests
+    {
+        [Theory]
+        [InlineData("Blog", "Blogs")]
+        [InlineData("Inventory", "Inventories")]
+        [InlineData("", "")]
+        public void NavigateUrlTest(string singular, string expected)
+        {
+            // Arrange
+            var actual = string.Empty;
+
+            // Act
+            actual = Pluralization.Pluralize(singular);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+
+}
