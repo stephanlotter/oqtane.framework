79611383a64b90e4c164ea9fa02c758cb16f37c1
Stephan Lotter <stephan@neurasoft.co.za>
2020-05-22 10:46:26 +0200

    Fix. Input containing both plural and singular [Module] tokens only replaced the first occurence.

--- Oqtane.Shared/Shared/Pluralization.cs
+++ Oqtane.Shared/Shared/Pluralization.cs
@@ -1,32 +1,37 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace Oqtane.Shared 
+namespace Oqtane.Shared
 {
-    public static class Pluralization 
+    public static class Pluralization
     {
         public static string ReplaceModuleWithPlural(string input, string singular)
         {
             var regexPlural = new Regex(@"\[Module\]s");
             var regexSingular = new Regex(@"\[Module\]");
-            var match = regexPlural.Match(input);
-            return match.Success ? regexPlural.Replace(input, Pluralize(singular)) : regexSingular.Replace(input, singular);
+
+            var matchPlural = regexPlural.Match(input);
+            input =
+                matchPlural.Success
+                    ? regexPlural.Replace(input, Pluralize(singular))
+                    : input;
+
+            var matchSingular = regexSingular.Match(input);
+            input =
+                matchSingular.Success
+                    ? regexSingular.Replace(input, singular)
+                    : input;
+
+            return input;
         }
 
-        public static string Pluralize(string singular)
-        {
-            return
-                string.IsNullOrWhiteSpace(singular)
-                    ? ""
-                    : singular.EndsWith("y")
-                        ? PluralizeY(singular)
-                        : singular + "s";
-        }
+        public static string Pluralize(string singular) =>
+            string.IsNullOrWhiteSpace(singular)
+                ? ""
+                : singular.EndsWith("y")
+                    ? PluralizeY(singular)
+                    : singular + "s";
 
-        private static string PluralizeY(string singular) 
-        {
-            return Regex.Replace(singular, @"y$", "ies");
-        }
-
+        private static string PluralizeY(string singular) => Regex.Replace(singular, @"y$", "ies");
     }
 
 }
--- Oqtane.Test/Oqtane.Shared.Tests/PluralizationTests.cs
+++ Oqtane.Test/Oqtane.Shared.Tests/PluralizationTests.cs
@@ -27,6 +27,8 @@ namespace Oqtane.Test.Oqtane.Shared.Tests
         [InlineData("Test input [Module]s value", "Inventory", "Test input Inventories value")]
         [InlineData("Test.input.[Module].value", "Inventory", "Test.input.Inventory.value")]
         [InlineData("Test input [Module] value", "Inventory", "Test input Inventory value")]
+        [InlineData("@inject I[Module]Service [Module]Service", "Inventory", "@inject IInventoryService InventoryService")]
+        [InlineData("                [Module] [Module] = new [Module]();", "Inventory", "                Inventory Inventory = new Inventory();")]
         [InlineData("", "", "")]
         public void ReplaceModuleWithPluralTest(string input, string singular, string expected)
         {
@@ -40,7 +42,91 @@ namespace Oqtane.Test.Oqtane.Shared.Tests
             Assert.Equal(expected, actual);
         }
 
+        [Fact]
+        public void ReplaceModuleWithPluralMultiLineSingleAndPluralTest()
+        {
+            var singular = "Inventory";
+            var input = @"            if (PageState.Action == 'Edit')
+            {
+                [Module] [Module]s[];
+                _id = Int32.Parse(PageState.QueryString['id']);
+                [Module] [Module] = await [Module] Service.Get[Module]Async(_id);
+                if ([Module] != null)
+                {
+                    _name = [Module].Name;
+                    _createdby = [Module].CreatedBy;
+                    _createdon = [Module].CreatedOn;
+                    _modifiedby = [Module].ModifiedBy;
+                    _modifiedon = [Module].ModifiedOn;
+                }
+            }";
 
+            var expected = @"            if (PageState.Action == 'Edit')
+            {
+                Inventory Inventories[];
+                _id = Int32.Parse(PageState.QueryString['id']);
+                Inventory Inventory = await Inventory Service.GetInventoryAsync(_id);
+                if (Inventory != null)
+                {
+                    _name = Inventory.Name;
+                    _createdby = Inventory.CreatedBy;
+                    _createdon = Inventory.CreatedOn;
+                    _modifiedby = Inventory.ModifiedBy;
+                    _modifiedon = Inventory.ModifiedOn;
+                }
+            }";
+
+            // Arrange
+            var actual = string.Empty;
+
+            // Act
+            actual = Pluralization.ReplaceModuleWithPlural(input, singular);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReplaceModuleWithPluralMultiLineTest()
+        {
+            var singular = "Inventory";
+            var input = @"            if (PageState.Action == 'Edit')
+            {
+                _id = Int32.Parse(PageState.QueryString['id']);
+                [Module] [Module] = await [Module] Service.Get[Module]Async(_id);
+                if ([Module] != null)
+                {
+                    _name = [Module].Name;
+                    _createdby = [Module].CreatedBy;
+                    _createdon = [Module].CreatedOn;
+                    _modifiedby = [Module].ModifiedBy;
+                    _modifiedon = [Module].ModifiedOn;
+                }
+            }";
+
+            var expected = @"            if (PageState.Action == 'Edit')
+            {
+                _id = Int32.Parse(PageState.QueryString['id']);
+                Inventory Inventory = await Inventory Service.GetInventoryAsync(_id);
+                if (Inventory != null)
+                {
+                    _name = Inventory.Name;
+                    _createdby = Inventory.CreatedBy;
+                    _createdon = Inventory.CreatedOn;
+                    _modifiedby = Inventory.ModifiedBy;
+                    _modifiedon = Inventory.ModifiedOn;
+                }
+            }";
+
+            // Arrange
+            var actual = string.Empty;
+
+            // Act
+            actual = Pluralization.ReplaceModuleWithPlural(input, singular);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
     }
 
 }
